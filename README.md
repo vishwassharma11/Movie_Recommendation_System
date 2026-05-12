60994d6ce6ad45fea63827aa0c8295a910c6467c
vishwas sharma <168301559+vishwassharma11@users.noreply.github.com>
2026-05-13 01:33:47 +0530

    Add files via upload

--- README.md
+++
@@ -1,2 +0,0 @@
-# Movie_Recommendation_System
-Built a machine learning-based movie recommendation system using content-based filtering and cosine similarity to suggest similar movies based on user input. Performed data preprocessing, feature extraction, and similarity analysis using Python, Pandas, and Scikit-learn.
---
+++ app.py
@@ -0,0 +1,43 @@
+
+
+import pandas as pd
+import streamlit as st
+import pickle
+import numpy as np
+
+def recommend(movie):
+    movie_index = movies[movies['title_x'] == movie].index[0]
+    distances = similarity[movie_index]
+
+    movies_list = sorted(
+        list(enumerate(distances)),
+        reverse=True,
+        key=lambda x: x[1]
+    )[1:6]
+
+    recommended_movies = []
+
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title_x)
+
+    return recommended_movies
+
+
+st.title('Movie review system')
+st.header('Movie review system')
+
+movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+option = st.selectbox(
+    "Select a movie",
+    movies['title_x'].values
+)
+
+if st.button("Recommend"):
+    recommendations = recommend(option)
+
+    for movie in recommendations:
+        st.write(movie)
